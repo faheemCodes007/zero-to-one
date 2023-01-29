d04519253b1bb8e4ad35170cd77ed88b960ddeef
Mr. Abdullah <akjee204@gmail.com>
2023-01-30 01:15:52 +0500

    fixed nav bar on / and article info container on /article page

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-const express = require("express")
-const app = express()
-const http = require("http").createServer(app)
-const Article = require("./model/article")
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const Article = require("./model/article");
+const mongoose = require("mongoose");
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/articles")
+mongoose.connect("mongodb://127.0.0.1:27017/articles");
 mongoose.connection.on("open", () => {
-    console.log("DB Connected.")
-})
+    console.log("DB Connected.");
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/static/"))
+app.use(express.static(__dirname + "/static/"));
 
 app.get("/article", async (req, res) => {
     let article = new Article({
-        title: "Why you should trust H&S Real Estate.",
+        title: "Why you should trust ERA Real Estate",
         author: "Mr. Abdullah",
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur fugit in illo nisi aut, facilis nostrum voluptatibus! Modi obcaecati aliquid rerum accusamus pariatur perspiciatis ipsa.",
-        thumbnail: "/images/hslogo.png"
-    })
-    article.save().then(result => {
-        console.log(result)
-        res.status(201).send(result)
-    }).catch(err => {
-        console.log(err)
-        res.status(500).send({error: err})
-    })
-})
+        description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur fugit in illo nisi aut, facilis nostrum voluptatibus! Modi obcaecati aliquid rerum accusamus pariatur perspiciatis ipsa.",
+        thumbnail: "/images/hslogo.png",
+    });
+    article
+        .save()
+        .then((result) => {
+            console.log(result);
+            res.status(201).send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send({ error: err });
+        });
+});
 
 app.get("/getAllArticles", (req, res) => {
-    Article.find().exec().then(doc => {
-        if (doc.length === 0) {
-            return res.status(404).send({message: "No articles found."})
-        }
-        return res.status(200).send(doc)
-    })
-})
+    Article.find()
+        .exec()
+        .then((doc) => {
+            if (doc.length === 0) {
+                return res.status(404).send({ message: "No articles found." });
+            }
+            return res.status(200).send(doc);
+        });
+});
 app.get("/article/:article", (req, res) => {
-    res.status(200).sendFile(__dirname + "/static/pages/article.html")
+    res.status(200).sendFile(__dirname + "/static/pages/article.html");
 
     // Article.find({title: req.params.article}).exec().then(doc => {
     //     res.status(200).send(doc)
@@ -47,17 +52,30 @@ app.get("/article/:article", (req, res) => {
     //     console.log(err)
     //     res.status(500).send({error: err})
     // })
-})
-
-
+});
+app.get("/getArticle", (req, res) => {
+    let { title } = req.query;
+    console.log(title);
+    Article.find({ title: title.replaceAll("-", " ") })
+        .exec()
+        .then((doc) => {
+            console.log(doc);
+            return res.status(200).send(doc[0]);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send({ error: err });
+        });
+    // res.status(200).send({ message: "Done" });
+});
 
 app.get("/", (req, res) => {
-    res.status(200).sendFile(__dirname + "/static/pages/index.html")
-})
+    res.status(200).sendFile(__dirname + "/static/pages/index.html");
+});
 app.use((req, res) => {
-    res.status(404).sendFile(__dirname + "/static/pages/pageNotFound.html")
-})
+    res.status(404).sendFile(__dirname + "/static/pages/pageNotFound.html");
+});
 
 http.listen(PORT, () => {
-    console.log(`Server is up on localhost:${PORT}`)
-})
+    console.log(`Server is up on localhost:${PORT}`);
+});
--- static/pages/index.js
+++ static/pages/index.js
@@ -1,15 +1,21 @@
-const FEATUREDARTICLES = document.querySelector("body > div > div.featured-container")
-const MAINBANNERBUTTON = document.querySelector("a#mainBannerButton")
+const FEATUREDARTICLES = document.querySelector(
+    "body > div > div.featured-container"
+);
+const MAINBANNERBUTTON = document.querySelector("a#mainBannerButton");
+const BANNER = document.querySelector("body > div.app > div.banner");
+const NAV = document.querySelector("body > nav");
 
 document.body.onload = async () => {
-    console.log("Here")
-    let articles = await fetch("/getAllArticles")
-    let json = await articles.json()
+    console.log("Here");
+    let articles = await fetch("/getAllArticles");
+    let json = await articles.json();
     for (let article of json) {
-        MAINBANNERBUTTON.setAttribute("href", `/article/${article.title.replaceAll(" ", "-")}`)
+        MAINBANNERBUTTON.setAttribute(
+            "href",
+            `/article/${article.title.replaceAll(" ", "-")}`
+        );
         FEATUREDARTICLES.innerHTML += `<div class="article-container">
-        <a href="/article/${article.title.replaceAll(" ", 
-        "-")}">
+        <a href="/article/${article.title.replaceAll(" ", "-")}">
             <img src=${article.thumbnail} alt="H&S Logo">
             <div class="info-container">
                 <h1>
@@ -21,6 +27,17 @@ document.body.onload = async () => {
             </div>
         </a>
     </div>
-    `
+    `;
     }
- }
+};
+
+document.body.onscroll = (ev) => {
+    let height = BANNER.clientHeight;
+    console.log(window.scrollY);
+    // console.log(window.s)
+    if (window.scrollY >= height) {
+        NAV.setAttribute("whiteBackground", "true");
+        return;
+    }
+    NAV.setAttribute("whiteBackground", "false");
+};
